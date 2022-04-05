37b0ae968967734b4583020b90ea09e645942116
fvjupiter <schoof.frederik@gmail.com>
2022-04-05 19:09:00 +0200

    added first button style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export interface ButtonProps {
   label: string;
+  classN: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className={`${props.classN} bg-lime-500`}>{props.label}</button>;
 };
 
 export default Button;
